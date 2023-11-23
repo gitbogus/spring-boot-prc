8c1da7f1cef643803bb483c5037ac4ffbf4fccb6
bogus <bogus0115@naver.com>
2023-11-23 16:10:27 +0900

    start spring boot

---
+++ src/test/java/com/campus/campus/SpringBootPrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
